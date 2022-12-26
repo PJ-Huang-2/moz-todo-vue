135366c3557659e34db612c632dbf8ba312f06c8
Nate <natehuang0913@gmail.com>
2022-12-27 00:56:12 +0800

    vue-todolist

---
+++ src/components/ToDoForm.vue
@@ -0,0 +1,44 @@
+<template>
+    <form>
+        <label for="new-todo-input" class="label__lg">
+            What needs to be done?
+        </label>
+        <input
+            type="text"
+            id="new-todo-input"
+            name="new-todo"
+            autocomplete="off"
+            v-model.lazy.trim="label"
+            class="input__lg"
+        />
+        <button
+            @click.prevent="onSubmit"
+            type="submit"
+            class="btn btn__primary btn__lg"
+        >
+            Add
+        </button>
+    </form>
+</template>
+
+<script>
+export default {
+    data() {
+        return {
+            label: "",
+        };
+    },
+    methods: {
+        onSubmit() {
+            // console.log("Label value:", this.label);
+            // this.$emit("todo-added");
+            // this.$emit("todo-added", this.label);
+            if (this.label === "") {
+                return;
+            }
+            this.$emit("todo-added", this.label);
+            this.label = "";
+        },
+    },
+};
+</script>
